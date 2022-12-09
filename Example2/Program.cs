6a92feae3c6be619942361443a1ec7198b6c4d50
Sapsan <semya_s@mail.ru>
2022-12-09 14:37:02 +0300

    Add Example2 (HelloUserName)

---
+++ Example2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите Ваше имя"); //Write выводит строку с % без перехода на следующую строку
+string usrname = Console.ReadLine();//присвоение переменной (тип строка) поле ввода
+Console.WriteLine("Привет,");//WriteLine выводит строку и переходит на следующую
+Console.WriteLine(usrname);
